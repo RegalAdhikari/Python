d1f1f1c48ee929c9ac88e0c0f8cd904512da10ef
Regal Adhikari <65963700+RegalAdhikari@users.noreply.github.com>
2023-08-29 09:47:31 +0545

    Worked On Conditionals

--- 04_String_Playground.py
+++ 04_String_Playground.py
@@ -1,4 +1,8 @@
-#let's see what we can do with strings
+# let's see what we can do with strings
 hello = "Hello"
 print(type(hello))
 print(hello.upper())
+# lets count the number of l's
+print(hello.count('l'))
+jpt = "CaPiTaLIZeMe"
+print(jpt.capitalize())
---
+++ 05_Conditional_Operators.py
@@ -0,0 +1,7 @@
+x = "hello"
+y = "HeLlo"
+print(x != y)
+print('a' > 'Z')
+print(ord('a'))
+result = 6 == 6
+print(result)
